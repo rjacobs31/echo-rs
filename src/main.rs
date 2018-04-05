3e9fb6cfd0da1a57a2549b7d5b36698a43722e9a
Rudolph Jacobs <jacobsrudolph@yahoo.com>
2018-04-05 19:23:26 +0200

    Switch to Tokio package for TCP streams.

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,45 @@
-extern crate futures;
-extern crate hyper;
+extern crate tokio;
 
-use futures::future::Future;
-use hyper::header::ContentLength;
-use hyper::server::{Http, Request, Response, Service};
-use hyper::{Method, StatusCode};
+use std::env::args;
+
+use tokio::io::copy;
+use tokio::net::TcpListener;
+use tokio::prelude::*;
 
 fn main() {
-    let addr = "127.0.0.1:3000".parse().unwrap();
-    let server = Http::new().bind(&addr, || Ok(Echo)).unwrap();
-    server.run().unwrap()
-}
+    // fetch server address from first arg
+    let addr = args().nth(1).unwrap_or("127.0.0.1:3000".to_string());
 
-struct Echo;
+    // parse server address
+    let addr = addr.parse().expect("unable to parse server address");
 
-impl Service for Echo {
-    type Request = Request;
-    type Response = Response;
-    type Error = hyper::Error;
+    // bind TCP listener to address
+    let listener = TcpListener::bind(&addr).expect("unable to bind TCP listener");
+    println!("server successfully started at {}", &addr);
 
-    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
+    // initialise listener
+    let server = listener
+        .incoming()
+        .map_err(|e| eprintln!("accept failed = {:?}", e))
+        .for_each(|sock| {
+            // split reader/writer of socket
+            let (reader, writer) = sock.split();
 
-    fn call(&self, req: Request) -> Self::Future {
-        let mut response = Response::new();
+            // obtain a future which will copy input to output
+            let bytes_copied = copy(reader, writer);
+            let handle_conn = bytes_copied
+                .map(|_amt| {
+                    // safely ignore number of bytes read on success
+                })
+                .map_err(|e| {
+                    // report on error
+                    eprintln!("IO error {:?}", e)
+                });
 
-        match (req.method(), req.path()) {
-            (&Method::Get, "/") => {
-                response.set_body("Try POSTing data to /echo");
-            }
-            (&Method::Post, "/echo") => {
-                response.set_body(req.body());
-            }
-            _ => {
-                response.set_status(StatusCode::NotFound);
-            }
-        };
+            // spawn the socket to handle connections
+            tokio::spawn(handle_conn)
+        });
 
-        Box::new(futures::future::ok(response))
-    }
+    // run the server with the given listener
+    tokio::run(server)
 }
